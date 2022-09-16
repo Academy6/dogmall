077f08b45b5e0c9c4b22552e0a8c35c4da60517b
C팀 <sywon2005@gmail.com>
2022-09-16 18:58:11 +0900

    아이디 체크 정규식

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -17,14 +17,15 @@ const Login = ()=> {
         const regex = /^[a-z]+[a-z0-9]{5,19}$/g;
         console.log(`ID:${loginID}\nPassword:${loginPassword}`)
         console.log(regex.test(loginID))
-        // if (regex.test(event.target.value) === false) {
+        event.preventDefault()
+        if (regex.test(loginID) === true) {
+            alert(`ID:${loginID}\nPassword:${loginPassword}`)
+        }
+        // if (regex.test(loginID) === false) {
         //     alert('id를 올바르게 써주세요')
-        // }
-        // if (LoginPassword === '') {
-        //     alert("비밀번호를 입력해주세요")
-        // }
-        // if(LoginID.length <= 5) {
-        //     console.log(LoginID.length)
+        // } else if (regex.test(loginID) === true) {
+        //     alert(`ID:${loginID}\nPassword:${loginPassword}`)
+        //     event.preventDefault()
         // }
     }
     return (
@@ -32,9 +33,11 @@ const Login = ()=> {
             Log In TEST
             <form>
                 <input type='text' placeholder='아이디' onChange={onChangeID} value={loginID} required  />   
-                <input type='password' placeholder='비밀번호' onChange={onChangePassword} value={loginPassword} required />   
+                <input type='password' placeholder='비밀번호' onChange={onChangePassword} value={loginPassword} required />  
             </form>
-            <button onClick={onClick}>로그인</button>
+            <form>
+                <button onClick={onClick}>로그인</button>
+            </form>
             <Link to="/option" className='text-link' ><p>회원가입</p></Link>
         </div>
     );
