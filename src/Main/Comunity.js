6b82082140f5baf70b2142445fd2c2002b2fd4ba
C팀 <sywon2005@gmail.com>
2022-09-28 21:13:50 +0900

    scrollbar 생성

--- src/Main/Comunity.js
+++ src/Main/Comunity.js
@@ -1,6 +1,7 @@
 import React, { Component, useEffect } from 'react';
 import { useState } from 'react';
 import { dbService } from '../fbase'
+import styled from 'styled-components';
 import WriteButton from './WriteButton';
 import "../css/comunity.css"
 
@@ -38,9 +39,13 @@ const  Comunity = ({userObj})=> {
     return (
         <div>
             <div className='comunity'>
-                {writes.map((write)=> (
-                    <WriteButton key={write.id} writeObj={write} isOwner={write.creatorId === userObj.email} />
-                ))}
+                <StyledAllwaysScrollSection>
+                    <div>
+                        {writes.map((write)=> (
+                            <WriteButton key={write.id} writeObj={write} isOwner={write.creatorId === userObj.email} />
+                        ))}
+                    </div>
+                </StyledAllwaysScrollSection>
             </div>
             <form onSubmit={onSubmit}>
                 <input value={write} onChange={onChange} type='text' placeholder='작성해주세요' maxLength={200} />
@@ -49,5 +54,19 @@ const  Comunity = ({userObj})=> {
         </div>
     );
 }
+const StyledAllwaysScrollSection = styled.div`
+    overflow: scroll;
+    height: 500px;
+    &::-webkit-scrollbar {
+        width: 8px;
+        height: 8px;
+        border-radius: 6px;
+        background-color: rgb(255, 255, 255, 0.4);
+    }
+    &::-webkit-scrollbar-thumb {
+        background-color: rgb(0, 0, 0, 0.3);
+        border-radius: 6px;
+    }
+`
 
 export default  Comunity;
