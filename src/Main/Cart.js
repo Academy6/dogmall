a850890f2dd69041b3dc32eb54e3dee85493cff0
C팀 <sywon2005@gmail.com>
2022-09-28 20:39:57 +0900

    장바구니 상태 변경

--- src/Main/Cart.js
+++ src/Main/Cart.js
@@ -8,7 +8,17 @@ function Cart(props){
 
     return(
       <div>
-        gf
+        <Table>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>상품명</th>
+              <th>가격</th>
+              <th>수량</th>
+              <th>변경</th>
+            </tr>
+          </thead>
+        </Table>
       </div>
       // <div>
       //     <Table>
