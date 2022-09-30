b0dc3b1dd1afdbae6f15bb2361f982bbe9ec4849
C팀 <sywon2005@gmail.com>
2022-09-30 20:45:55 +0900

    업로드 시작

--- src/Upload/Upload.js
+++ src/Upload/Upload.js
@@ -1,17 +1,93 @@
-import { Form, Divider, Input, InputNumber, Button } from 'antd';
+import React, { useCallback } from 'react'
+import { Form, Divider, Input, InputNumber, Button, List } from 'antd';
+// import '../scss/upload.css';
 import 'antd/dist/antd.css';
+import { useState } from 'react';
+import { dbService } from '../fbase'
 
-const Upload = (props) => {
 
+const Upload = (userObj) => {
+//     const value = {
+//         seller: seller,
+//         name: name,
+//         price: price,
+//         description: description,
+//         imgupload: imgupload  
+//       }
+  
+  
+    const file = null
     
+    const useInput = (userObj) => {
+        const [value, setValue] = useState(userObj);
+        const handler = (event) => {
+          setValue(event.target.value);
+        };
+        return [value, handler];
+      }
+    const [form] = Form.useForm();
+    const [imgupload, setImgUpload] = useInput('');
+    const [seller, setSeller] = useInput('');
+    const [name, setName] = useInput('');
+    const [price, setPrice] = useInput('');
+    const [description, setDescription] = useInput('');
+    
+    const onChangeImage = ((event) => {
+        
+        file = event.target.files[0];
+        console.log("file", file)
+    })
+    const onsubmitForm = useCallback(({ imgUpload, seller , name, price, description }) => {
+       
+
+        const formData = new FormData()
+    
+        
+        formData.append( "seller", seller)
+        formData.append( "name", name)
+        formData.append( "price", price)
+        formData.append( "description", description)
+        formData.append(  "imgUpload", imgUpload  )
+       
+        
+        for(  let str  of formData ) {
+            console.log( str   )
+        }
+        
+    })
+    
+    // const onClick = async()=> {
+    //     const db = dbService
+    //     await db.collection("Name").add({ 
+    //         text: form ,
+    //         createdAt: Date.now(),
+    //         creatorId: form.email
+    //     })
+    //     form("")
+    // }
+
+    const onClick = async () => {
+        
+        await dbService.collection('user').add({
+          text: form,
+          createdAt: Date.now(),
+          creatorId: userObj.uid, // userObj: props로 넘겨준 login한 user 정보
+        });
+        form('');
+      };
+        // 파이어베이스 -> formData 넘겨줌 ->  파이어베이스 응답 ->  응답에 따라 처리(성공/실패) -> 성공 : 상세페이지로 이동 / 메인페이지로 이동 
+        //                                                                                      -> 실패 : 무슨문젠지 알림            
+
+ 
     return (
-        <div id="upload-container" className='inner'>
-            <Form name="productUpload">
+        <div id="upload-container" className='inner'>            
+            <Form name="productUpload" onFinish={onsubmitForm} form={form}>  
                 <Form.Item name="imgUpload"
                     label={<div className='upload-label'>상품사진</div>}>
                     <div id="upload-img-placeholder">
                         <img src="images/icons/camera.png" alt="" />
                         <span>이미지를 업로드 해주세요.</span>
+                        <input type={"file"}  onChange={onChangeImage} />
                     </div>
                 </Form.Item>
                 <Divider/>
@@ -44,9 +120,10 @@ const Upload = (props) => {
                 />
                 </Form.Item>
                 <Form.Item>
-                    <Button id="submit-button" size="large" htmlType='submit'>
+                    <Button id="submit-button" size="large" htmlType='submit' onClick={onClick} >
                         상품등록하기
                     </Button>
+                    
                 </Form.Item>
             </Form>
         </div>
