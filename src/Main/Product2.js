8b8d2dbf740c1fd199175a8339a76585e45ecf1f
C팀 <sywon2005@gmail.com>
2022-10-03 23:22:45 +0900

    상품정보 받아오기

--- src/Main/Product2.js
+++ src/Main/Product2.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { dbService } from '../fbase';
 
 
-const Product2 = (props) => {
+const Product2 = ({userObj}) => {
+    const Test = ()=> {
+        const data = userObj
+        const test = dbService.collection('goodsInfo').onSnapshot(snapshot=> {
+            const goodsInfoArray = snapshot.docs.map((doc)=> ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            console.log(goodsInfoArray)
+        })
+    }
     return (
-        <>
-            <h3>상품진열페이지입니다.</h3>
-        </>
+        <div>
+            <button onClick={Test}>test</button>
+        </div>
     );
 }
 
