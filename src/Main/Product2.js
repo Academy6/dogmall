27009109a7ee9ba375dd3d272090aeed71f5d3c9
C팀 <sywon2005@gmail.com>
2022-10-04 07:05:08 +0900

    fbase값 가져오기

--- src/Main/Product2.js
+++ src/Main/Product2.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { dbService } from '../fbase';
 
 
 const Product2 = () => {
+    const Test = ()=> {
+        dbService.collection('goodsInfo').onSnapshot(snapshot=> {
+            const goodsInfoArray = snapshot.docs.map((doc)=> ({
+                id: doc.id,
+                ...doc.data()
+            }))
+            console.log(goodsInfoArray)
+        })
+    }
     return (
         <div>
+            <button onClick={Test}>test</button>
             상품보기 페이지
         </div>
-    );
+    );    
 }
 
 export default Product2;
