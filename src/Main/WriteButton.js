f93454a7d6b8917ae516d59eb2fe56090dfc2d9f
C팀 <sywon2005@gmail.com>
2022-10-07 21:07:06 +0900

    본인이 쓴 글만 오른쪽 정렬 클래스 부여

--- src/Main/WriteButton.js
+++ src/Main/WriteButton.js
@@ -2,11 +2,19 @@ import React, { Component, useEffect, useState } from 'react';
 import { dbService } from '../fbase';
 import '../css/Link.css'
 
-const WriteButton = ({ writeObj, isOwner })=> {
+const WriteButton = ({ writeObj, isOwner, userObj })=> {
     const [editing, setEditing] = useState(false)
     const [newWrite, setNewWrite] = useState(writeObj.text)
+    const [isUser, setIsUser] = useState(false)
     const date = new Date()
-    // console.log(writes)
+    // console.log(userObj)
+    useEffect(()=> {
+        if (writeObj.creatorId === userObj.email) {
+            setIsUser(true)
+        } else {
+            setIsUser(false)
+        }
+    }, [])
 
     const onDeleteClick = async()=> {
         const ok = window.confirm(`정말로 삭제하시겠습니까?`)
@@ -38,21 +46,21 @@ const WriteButton = ({ writeObj, isOwner })=> {
         <div>
             {
                 editing ? (
-                    <>
+                    <div className={isUser ? 'isUser' : 'notUser'}>
                         <form onSubmit={onSubmit}>
                             <input type="text" placeholder="수정하세요" value={newWrite} required onChange={onChange} />
                             <button onClick={onClick}>게시</button>
                         </form>
                         <button onClick={toggleEditing}>Cancel</button>
-                    </>
+                    </div>
                 ) : (
-                    <div>
+                    <div className={isUser ? 'isUser' : 'notUser'}>
                         <h4>{writeObj.text}</h4>
-                        <h6>{writeObj.time}</h6>
+                        <h6>{writeObj.time}</h6> 
                         {isOwner && (
                             <div>
-                                <button onClick={onDeleteClick}>삭제</button>
                                 <button onClick={toggleEditing}>수정</button>
+                                <button onClick={onDeleteClick}>삭제</button>
                             </div>
                         )}
                     </div>
