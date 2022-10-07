dc0951f2967237153a24261b394ceca7d7e84a47
C팀 <sywon2005@gmail.com>
2022-10-08 00:07:02 +0900

    정리

--- src/index.js
+++ src/index.js
@@ -6,8 +6,5 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    // <Provider store={store}>
-    //     <App />
-    // </Provider>
     <App />
 );
