13c4ff1adf0982f5f57855adda6c2fcd28c7e38c
C팀 <sywon2005@gmail.com>
2022-10-09 03:55:24 +0900

    정리

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { UserOutlined, UserAddOutlined } from '@ant-design/icons';
 import { authService } from '../fbase'
 import {NavLink} from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
