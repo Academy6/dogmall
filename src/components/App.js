259b55c7737096bad207fb19ea237bcebe0b9c93
C팀 <sywon2005@gmail.com>
2022-10-10 18:35:18 +0900

    footer 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import  Router  from './Router';
 import '../App.css';
+import Footer from '../Footer/footer';
 
 function App() {
   return (
     <div>
       <Router />
+      <Footer />
     </div>
   );
 }
