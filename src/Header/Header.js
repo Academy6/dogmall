d9da8402ea2b0dfd2e0e9fd68e1dc04934feb2db
C팀 <sywon2005@gmail.com>
2022-10-11 22:47:44 +0900

    메뉴바 생성

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,104 +4,112 @@ import { useNavigate } from 'react-router-dom';
 import { authService } from '../fbase'
 import '../scss/custom.scss'
 import {AlignCenterOutlined} from '@ant-design/icons'
+
+
 const Header = ({isLoggedIn})=> {
     const navigate = useNavigate()
-    const [toggleMenu, setToggleMenu] = useState(false)
-    const [toggleBar, setToggleBar] = useState(true)
+    const [isToggled, setIsToggled] = useState(false);
 
     const onClickLogOut = ()=> {
         authService.onAuthStateChanged((user)=> {
             if(user) {
-                authService.signOut()
                 navigate("/")
+                authService.signOut()
             }
         })
     }
-    const onMenuClick = () => {
-        setToggleMenu(!toggleMenu)
-        setToggleBar(!toggleBar)
-      }
+    const onClickFunc = ()=> {
+        authService.onAuthStateChanged((user)=> {
+            if (user) {
+
+            } else {
+                const ok = window.confirm(`로그인이 필요한 화면입니다. 로그인하시겠습니까?`)
+                if (ok === true) {
+                    navigate('/login')
+                } else {
+                    navigate('/')
+                }
+            }
+        })
+    }
+    const onClickBar = ()=> {
+        setIsToggled(!isToggled)
+        console.log('gd')
+    }
+   
     return (
-		<div>
-            {/* Navbar */}
-            <div id="header">
-                <h1><NavLink to ='/' className='text-link petshop'>PetShop</NavLink></h1>
-                <div className="inner">
-                    {isLoggedIn ?(
-                        <nav className='nav'>
-                            <h1><NavLink to ='/' className='text-link petshop'>PetShop</NavLink></h1>
-                            <ul className='navbar'>
-                                <li><NavLink to ='/upload/2' className='text-link'>상품등록하기</NavLink></li>
-                                <li><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
-                                <li><NavLink to ='/comunity/4' name='comunity' className='text-link'>커뮤니티</NavLink></li>
-                                <li><NavLink to ='/cart/5' className='text-link'>장바구니</NavLink></li>
-                                {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
-                                <li><NavLink onClick={onClickLogOut} className='text-link'>logout</NavLink></li>
-                            </ul>
-                         </nav>
+        <div>
+    {/* Navbar */}
+    
+            <nav id="navbar" className="navBar">
+                <h1 className='navBar_main'><NavLink to ='/' className='text-link'>PetShop</NavLink></h1>
+                <div className='no-bar'>
+                    {isLoggedIn ? (
+                            <div>
+                                <ul className="navBar_menus">
+                                    <li className="navBar_menus_menu"><NavLink to ='/upload/2' className='text-link'>상품등록하기</NavLink></li>
+                                    <li className="navBar_menus_menu"><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
+                                    <li className="navBar_menus_menu"><NavLink to ='/comunity/4' name='comunity' className='text-link'>커뮤니티</NavLink></li>
+                                    <li className="navBar_menus_menu"><NavLink to ='/cart/5' className='text-link'>장바구니</NavLink></li>
+                                    {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
+                                    <li className="navBar_menus_menu"><NavLink onClick={onClickLogOut} className='text-link'>logout</NavLink></li>
+                                </ul>
+                                <ul className="bavBar_icons">
+                                    <AlignCenterOutlined />
+                                </ul>
+                            </div>
                     ) : (
-                        <nav className='nav'>
-                            <ul className='navbar'>
-                                <h1><NavLink to ='/' className='text-link petshop'>PetShop</NavLink></h1>
-                                <li><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
+                        <div>
+                            <ul>
+                                <li className="navBar_menus_menu"><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
                                 {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
-                                <li><NavLink to="/login" className='text-link'>login</NavLink></li>
-                                <li><NavLink to="/signup" className='text-link' >회원가입</NavLink></li>
+                                <li className="navBar_menus_menu"><NavLink to="/login" className='text-link'>login</NavLink></li>
+                                <li className="navBar_menus_menu"><NavLink to="/signup" className='text-link' >회원가입</NavLink></li>
                             </ul>
-                        </nav>
+                        </div>
                     )}
                 </div>
-                <div className='bar'>
-                    {toggleMenu ? (
-                        <div className='login-bar'>
-                            {isLoggedIn ?(
-                                <menu inlineCollapsed={toggleBar}
-                                onClick={onMenuClick}>
-                                    <nav className='nav'>
-                                        <ul className='navbar'>
-                                            <li><NavLink to ='/upload/2' className='text-link'>상품등록하기</NavLink></li>
-                                            <li><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
-                                            <li><NavLink to ='/comunity/4' name='comunity' className='text-link'>커뮤니티</NavLink></li>
-                                            <li><NavLink to ='/cart/5' className='text-link'>장바구니</NavLink></li>
+                <div>
+                    {isToggled ? (
+                        <div>
+                            <ul className="bavBar_icons">
+                                 <AlignCenterOutlined onClick={onClickBar} />
+                            </ul>
+                            <div className={isToggled ? 'active-bar' : 'hidden-bar'}>
+                                {isLoggedIn ? (
+                                    <div>
+                                        <ul className="navBar_menus">
+                                            <li className="navBar_menus_menu"><NavLink to ='/upload/2' className='text-link'>상품등록하기</NavLink></li>
+                                            <li className="navBar_menus_menu"><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
+                                            <li className="navBar_menus_menu"><NavLink to ='/comunity/4' name='comunity' className='text-link'>커뮤니티</NavLink></li>
+                                            <li className="navBar_menus_menu"><NavLink to ='/cart/5' className='text-link'>장바구니</NavLink></li>
                                             {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
-                                            <li><NavLink onClick={onClickLogOut} className='text-link'>logout</NavLink></li>
+                                            <li className="navBar_menus_menu"><NavLink onClick={onClickLogOut} className='text-link'>logout</NavLink></li>
                                         </ul>
-                                    </nav>
-                                </menu>
-                            ) : (
-                                <nav className='nav'>
-                                    <ul className='navbar'>
-                                        <li><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
-                                        {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
-                                        <li><NavLink to="/login" className='text-link'>login</NavLink></li>
-                                        <li><NavLink to="/signup" className='text-link' >회원가입</NavLink></li>
-                                    </ul>
-                                </nav>
-                            )}
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <ul className='navBar_menus'>
+                                            <li className="navBar_menus_menu"><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
+                                            {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
+                                            <li className="navBar_menus_menu"><NavLink to="/login" className='text-link'>login</NavLink></li>
+                                            <li className="navBar_menus_menu"><NavLink to="/signup" className='text-link' >회원가입</NavLink></li>
+                                        </ul>
+                                    </div>
+                                )}
+                            </div>
                         </div>
                     ) : (
-                        <div className='not-login-bar'>
-                            <ul onClick={onMenuClick} className="bavBar_icons">
-                                <li></li>
-                                <li></li>
-                                <li></li>
+                        <div>
+                            <ul className="bavBar_icons">
+                                 <AlignCenterOutlined onClick={onClickBar} />
                             </ul>
                         </div>
                     )}
                 </div>
-            </div>   
+            </nav>   
         </div>
     );
 }
 
 export default Header;
-
-
-
-
-{/* <ul className="togleMenus">
-                                <li className="toggle_menu"><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
-                                <li className="toggle_menu"></li>
-                                <li className="toggle_menu"><NavLink to="/login" className='text-link'>login</NavLink></li>
-                                <li className="toggle_menu"><NavLink to="/signup" className='text-link' >회원가입</NavLink></li>
-                            </ul> */}
