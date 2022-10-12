81a553aac1b55c2fd8e3b5de000cb06ef59f88ed
C팀 <sywon2005@gmail.com>
2022-10-13 02:05:01 +0900

    navBar 디자인

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -34,29 +34,37 @@ const Header = ({isLoggedIn})=> {
     return (
         <div>
     {/* Navbar */}
-    
             <nav id="navbar" className="navBar">
                 <div className='no-bar'>
-                    <NavLink to ='/' className='text-link'><h1 className='navBar_main'>PetShop</h1></NavLink>
                     {isLoggedIn ? (
-                            <div>
-                                <ul className="navBar_menus">
-                                    <li className="navBar_menus_menu"><NavLink to ='/upload/2' className='text-link'>상품등록하기</NavLink></li>
-                                    <li className="navBar_menus_menu"><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
-                                    <li className="navBar_menus_menu"><NavLink to ='/comunity/4' name='comunity' className='text-link'>커뮤니티</NavLink></li>
-                                    <li className="navBar_menus_menu"><NavLink to ='/cart/5' className='text-link'>장바구니</NavLink></li>
-                                    {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
-                                    <li className="navBar_menus_menu"><NavLink onClick={onClickLogOut} className='text-link'>logout</NavLink></li>
-                                </ul>
+                        <div className='navBar_menus'>
+                            <div className='start-menu'><NavLink to ='/' className='text-link'><h1 className='navBar_main'>PetShop</h1></NavLink></div>
+                            <div className='center-menus'>
+                                <div className="navBar_menus_menu"><NavLink to ='/upload/2' className='text-link'>상품등록하기</NavLink></div>
+                                <div className="navBar_menus_menu"><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></div>
+                                <div className="navBar_menus_menu"><NavLink to ='/comunity/4' name='comunity' className='text-link'>커뮤니티</NavLink></div>
+                                <div className="navBar_menus_menu"><NavLink to ='/cart/5' className='text-link'>장바구니</NavLink></div>
                             </div>
+                                    {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
+                            <div className='end-menus'>
+                                <div className="navBar_menus_menu"><NavLink onClick={onClickLogOut} className='text-link'>logout</NavLink></div>
+                            </div>
+                        </div>
                     ) : (
                         <div>
-                            <ul className="navBar_menus">
-                                <li className="navBar_menus_menu"><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></li>
+                            <div className="navBar_menus">
+                                <div className='start-menu'><NavLink to ='/' className='text-link'><h1 className='navBar_main'>PetShop</h1></NavLink></div>
+                                <div className='center-menus'>
+                                    <div><NavLink to ='/upload/2' className='text-link'>상품등록하기</NavLink></div>
+                                    <div><NavLink to ='/product2/3' className='text-link'>상품보기</NavLink></div>
+                                    <div><NavLink to ='/cart/5' className='text-link'>장바구니</NavLink></div>
+                                </div>
                                 {/* <li>{init ? <Router isLoggedIn={isLoggedIn} /> : '!'}</li> */}
-                                <li className="navBar_menus_menu"><NavLink to="/login" className='text-link'>login</NavLink></li>
-                                <li className="navBar_menus_menu"><NavLink to="/signup" className='text-link' >회원가입</NavLink></li>
-                            </ul>
+                                <div className='end-menus'>
+                                    <div><NavLink to="/login" className='text-link'>login</NavLink></div>
+                                    <div><NavLink to="/signup" className='text-link' >회원가입</NavLink></div>
+                                </div>
+                            </div>
                         </div>
                     )}
                 </div>
